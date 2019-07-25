b85c478c088f3df1c80e01b48d7d26cb96cec887
Maynor Peña <38294810+PENA98@users.noreply.github.com>
2019-07-24 20:20:21 -0600

    Commit inicial

---
+++ index.js
@@ -0,0 +1,42 @@
+// Importar los módulos de express
+const express = require('express');
+// Importar las rutas disponibles
+const routes = require('./routes');
+// Importar los módulos para direcciones (path)
+const path = require('path');
+// Importar los módulos para utilizar body parser
+const bodyParser = require('body-parser');
+// Importar los módulos para utilizar passport
+const passport = require('passport')
+
+const session = require("express-session")
+
+// Crear la conexión con la Base de Datos
+const db = require('./config/db');
+
+// Realizar la conexión
+// Sequelize se conecta mediante promises
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Promise
+db.sync()
+    .then(() => console.log('Conectado al servidor de BD'))
+    .catch(error => console.log(error));
+
+// Crear una App de express
+const app = express();
+
+// Desde dónde se cargan los archivos estáticos
+app.use(express.static('public'));
+
+// Habilitar Pug como nuestro Template Engine
+app.set('view engine', 'pug');
+
+// Habilitar BodyParser para leer los datos de los formularios
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Añadir la carpeta (ruta) que contiene las View (vistas)
+app.set('views', path.join(__dirname, './views'));
+
+app.use('/', routes());
+
+// Inicializar el servidor de express en un puerto
+app.listen(9999);
